68e292351e64c491b4c11aba7732461d7adb99e1
github username <github email address>
2019-11-03 21:42:35 -0800

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,24 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
-binding.pry
+l1 = Lifter.new("Ada", 100)
+l2 = Lifter.new("Bob", 200)
+l3 = Lifter.new("Cate", 300)
+
+# name
+g1 = Gym.new("L.A. Fitness")
+g2 = Gym.new("24 Hr. Fitness")
+g3 = Gym.new("Gold's Gym")
+
+# gym, lifter
+
+m1 = Membership.new(10, g1, l1)
+m2 = Membership.new(20, g2 ,l2)
+m3 = Membership.new(30, g3, l3)
+m4 = Membership.new(40, g1, l2)
+m5 = Membership.new(50, g1, l3)
+m6 = Membership.new(60, g2, l1)
+
+Pry.start
 
 puts "Gains!"
